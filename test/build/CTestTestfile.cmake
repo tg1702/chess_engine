c4932f07a40ac3a6f69abe3bf1086df1fe22d3d0
tg1702 <thelemagrannum@gmail.com>
2024-11-12 00:12:12 -0400

    Tracking build again

---
+++ test/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tg1117/chess_engine/test
+# Build directory: /home/tg1117/chess_engine/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/tg1117/chess_engine/test/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
