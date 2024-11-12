317fc776b52d4e0ad47bd9eedbc8acbc092794b8
tg1702 <thelemagrannum@gmail.com>
2024-11-12 00:11:06 -0400

    Removed build

--- test/build/hello_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/tg1117/chess_engine/test/build/hello_test[1]_tests.cmake")
-  include("/home/tg1117/chess_engine/test/build/hello_test[1]_tests.cmake")
-else()
-  add_test(hello_test_NOT_BUILT hello_test_NOT_BUILT)
-endif()
