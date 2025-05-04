0fd2e977e18bea854e91dfd892a5aa9b56e3309e
tg0217 <thelemagrannum@gmail.com>
2025-05-04 11:19:10 -0400

    Separate capture and quiet generation

--- src/movegen/movegen.cpp
+++ src/movegen/movegen.cpp
@@ -27,7 +27,7 @@ void MoveGen::generateMoves(MoveList* move_list){
 	generateBishopMoves<CAPTURES>(move_list);
 	generateQueenMoves<CAPTURES>(move_list);
 
-/* 
+
 	generateKingMoves<QUIETS>(move_list);
 	generateKnightMoves<QUIETS>(move_list);
 	
@@ -35,7 +35,7 @@ void MoveGen::generateMoves(MoveList* move_list){
 
 	generateRookMoves<QUIETS>(move_list);
 	generateBishopMoves<QUIETS>(move_list);
-	generateQueenMoves<QUIETS>(move_list); */
+	generateQueenMoves<QUIETS>(move_list);
 
 	
 
@@ -83,34 +83,47 @@ void MoveGen::generateKingMoves(MoveList* move_list){
         
 		while (king_bb != 0ULL){
             Square from = utils::pop_lsb(king_bb);
-		
+
 			uint64_t legalMove = kingLookups[from] & ~(state.pieces.all);
                 
-			addPossibleMove<CAPTURES>(from, legalMove, KING, move_list);  		 
-	}
+			if (Type == CAPTURES)
+			{
+				addPossibleMove<CAPTURES>(from, legalMove & state.enemies, KING, move_list);
+			}
+				
+			else
+			{
+				addPossibleMove<QUIETS>(from, legalMove & ~state.enemies, KING, move_list);	
 
-		if (state.whiteKSCastle)
-		{
-			move_list->moves[move_list->count] = Move(W_KS_CASTLE_FLAG, E1, G1, KING, KING);
-			move_list->count++;
+			}
+					 
 		}
 
-		if (state.whiteQSCastle)
-		{
-			move_list->moves[move_list->count] = Move(W_QS_CASTLE_FLAG, E1, C1, KING, KING);
-			move_list->count++;
-		}	
+		if (Type == QUIETS){
+			if (state.whiteKSCastle)
+			{
+				move_list->moves[move_list->count] = Move(W_KS_CASTLE_FLAG, E1, G1, KING, KING);
+				move_list->count++;
+			}
 
-		if (state.blackKSCastle)
-		{
-			move_list->moves[move_list->count] = Move(B_KS_CASTLE_FLAG, E8, G8, KING, KING);
-			move_list->count++;
+			if (state.whiteQSCastle)
+			{
+				move_list->moves[move_list->count] = Move(W_QS_CASTLE_FLAG, E1, C1, KING, KING);
+				move_list->count++;
+			}	
+
+			if (state.blackKSCastle)
+			{
+				move_list->moves[move_list->count] = Move(B_KS_CASTLE_FLAG, E8, G8, KING, KING);
+				move_list->count++;
+			}
+			if (state.blackQSCastle)
+			{
+				move_list->moves[move_list->count] = Move(B_QS_CASTLE_FLAG, E8, C8, KING, KING);
+				move_list->count++;
+			}
 		}
-		if (state.blackQSCastle)
-		{
-			move_list->moves[move_list->count] = Move(B_QS_CASTLE_FLAG, E8, C8, KING, KING);
-			move_list->count++;
-		}
+		
 	
 }
 
@@ -124,7 +137,16 @@ void MoveGen::generateKnightMoves(MoveList* move_list){
 		uint64_t legalMove = knightLookups[from] & ~(state.pieces.all);
 
 
-        addPossibleMove<CAPTURES>(from, legalMove,  KNIGHT, move_list);
+        if (Type == CAPTURES)
+		{
+			addPossibleMove<CAPTURES>(from, legalMove & state.enemies, KNIGHT, move_list);
+		}
+			
+		else
+		{
+			addPossibleMove<QUIETS>(from, legalMove & ~state.enemies, KNIGHT, move_list);	
+
+		}
 
  	}
 }
@@ -138,86 +160,84 @@ void MoveGen::generateWhitePawnMoves(MoveList* move_list){
     uint64_t promotions = state.pieces.pawn_bb & RANK_7;	
 
 
-		while (normal_moves != 0ULL){
-			Square from = utils::pop_lsb(normal_moves);
+		if (Type == QUIETS){
+			while (normal_moves != 0ULL){
+				Square from = utils::pop_lsb(normal_moves);
+		
+				uint64_t single_pawn_pushes = pawnLookups[WHITE][from] & empty;
+					   
+				uint64_t shift = single_pawn_pushes<<8;
+				uint64_t double_pawn_pushes = shift & empty & RANK_4;
+		
+		
+				uint64_t legalMove = single_pawn_pushes | double_pawn_pushes;
+						
+				while ( legalMove != 0ULL){
+					Square to = utils::pop_lsb(legalMove);
+					move_list->moves[move_list->count] = Move(NORMAL, from, to, PAWN);
+					move_list->count++;
+		
+				}
+		
+			}
+		
+			while (promotions != 0ULL){
+				Square from = utils::pop_lsb(promotions);
+				
+				uint64_t normal_promotions = pawnLookups[WHITE][from] & empty;
+				while (normal_promotions != 0ULL){
+					Square to = utils::pop_lsb(normal_promotions);
+					for(auto& normal_promoted: normal_promoted_codes){
+						move_list->moves[move_list->count] = Move(normal_promoted, from, to, PAWN);
+						move_list->count++;
+					}
+				}
+		
+				
+			}
+
+			while (ep_squares != 0ULL){	
+				Square from = utils::pop_lsb(ep_squares);
+				Square ep_square = static_cast<Square>(state.enPassant);
 	
-			uint64_t single_pawn_pushes = pawnLookups[WHITE][from] & empty;
-				   
-			uint64_t shift = single_pawn_pushes<<8;
-			uint64_t double_pawn_pushes = shift & empty & RANK_4;
-	
-	
-			uint64_t legalMove = single_pawn_pushes | double_pawn_pushes;
-					
-			while ( legalMove != 0ULL){
-				Square to = utils::pop_lsb(legalMove);
-				move_list->moves[move_list->count] = Move(NORMAL, from, to, PAWN);
+				move_list->moves[move_list->count] = Move(EN_PASSANT_FLAG, from, ep_square , PAWN, PAWN);
 				move_list->count++;
 	
 			}
 	
 		}
-	
-		while (promotions != 0ULL){
-			Square from = utils::pop_lsb(promotions);
-			
-			uint64_t normal_promotions = pawnLookups[WHITE][from] & empty;
-			while (normal_promotions != 0ULL){
-				Square to = utils::pop_lsb(normal_promotions);
-				for(auto& normal_promoted: normal_promoted_codes){
-					move_list->moves[move_list->count] = Move(normal_promoted, from, to, PAWN);
-					move_list->count++;
-				}
-			}
-	
-			
-		}
-
-	
-		normal_moves = state.pieces.pawn_bb & ~RANK_7;
-
-		while (normal_moves != 0ULL){
-			Square from = utils::pop_lsb(normal_moves);
-			uint64_t pawn_attack = (pawnAttackLookups[WHITE][from] & state.enemies );
-
-			addPossibleMove<CAPTURES>(from, pawn_attack, PAWN, move_list);
-			
-		}
-
-
-		promotions = state.pieces.pawn_bb & RANK_7;
-
-		while (promotions != 0ULL){
-			Square from = utils::pop_lsb(promotions);
-			uint64_t captures = pawnAttackLookups[WHITE][from] & state.enemies;
-			
-			while (captures != 0ULL)
-			{
-				Square to = utils::pop_lsb(captures);
-
-				for(auto& capture_promoted: capture_promoted_codes){
-					move_list->moves[move_list->count] = Move(capture_promoted, from, to, PAWN);
-					move_list->count++;
-				}	
-			}	
-			
-		}
-
 		
+		else {
 
-		while (ep_squares != 0ULL){	
-			Square from = utils::pop_lsb(ep_squares);
-			Square ep_square = static_cast<Square>(state.enPassant);
+			normal_moves = state.pieces.pawn_bb & ~RANK_7;
 
-			move_list->moves[move_list->count] = Move(EN_PASSANT_FLAG, from, ep_square , PAWN, PAWN);
-			move_list->count++;
+			while (normal_moves != 0ULL){
+				Square from = utils::pop_lsb(normal_moves);
+				uint64_t pawn_attack = (pawnAttackLookups[WHITE][from] & state.enemies );
 
+				addPossibleMove<CAPTURES>(from, pawn_attack, PAWN, move_list);
+				
+			}
+
+
+			promotions = state.pieces.pawn_bb & RANK_7;
+
+			while (promotions != 0ULL){
+				Square from = utils::pop_lsb(promotions);
+				uint64_t captures = pawnAttackLookups[WHITE][from] & state.enemies;
+				
+				while (captures != 0ULL)
+				{
+					Square to = utils::pop_lsb(captures);
+
+					for(auto& capture_promoted: capture_promoted_codes){
+						move_list->moves[move_list->count] = Move(capture_promoted, from, to, PAWN);
+						move_list->count++;
+					}	
+				}	
+				
+			}
 		}
-
-
-	
-
-	
 
 
 }
@@ -234,81 +254,86 @@ void MoveGen::generateBlackPawnMoves(MoveList* move_list){
 	//single and double pawn pushes
 	//
 
-			while (normal_moves != 0ULL){
-				Square from = utils::pop_lsb(normal_moves);
+	if (Type == QUIETS){
+		while (normal_moves != 0ULL){
+			Square from = utils::pop_lsb(normal_moves);
 
-				uint64_t single_pawn_pushes = pawnLookups[BLACK][from] & empty;
+			uint64_t single_pawn_pushes = pawnLookups[BLACK][from] & empty;
 
-				uint64_t shift = single_pawn_pushes>>8;
-				uint64_t double_pawn_pushes = shift & empty & RANK_5;
+			uint64_t shift = single_pawn_pushes>>8;
+			uint64_t double_pawn_pushes = shift & empty & RANK_5;
 
 
-				uint64_t legalMove = single_pawn_pushes | double_pawn_pushes;
+			uint64_t legalMove = single_pawn_pushes | double_pawn_pushes;
 
-				while ( legalMove != 0ULL){
-					Square to = utils::pop_lsb(legalMove);
-					move_list->moves[move_list->count] = Move(NORMAL, from, to, PAWN);
+			while ( legalMove != 0ULL){
+				Square to = utils::pop_lsb(legalMove);
+				move_list->moves[move_list->count] = Move(NORMAL, from, to, PAWN);
+				move_list->count++;
+
+			}
+		}
+
+
+		while (promotions != 0ULL){
+			Square from = utils::pop_lsb(promotions);
+			
+
+			uint64_t normal_promotions = pawnLookups[BLACK][from] & empty;
+			while (normal_promotions != 0ULL){
+				Square to = utils::pop_lsb(normal_promotions);
+				for(auto& normal_promoted: normal_promoted_codes){
+					move_list->moves[move_list->count] = Move(normal_promoted, from, to, PAWN);
 					move_list->count++;
-
 				}
 			}
 
+		}
 
-			while (promotions != 0ULL){
-				Square from = utils::pop_lsb(promotions);
-				
 
-				uint64_t normal_promotions = pawnLookups[BLACK][from] & empty;
-				while (normal_promotions != 0ULL){
-					Square to = utils::pop_lsb(normal_promotions);
-					for(auto& normal_promoted: normal_promoted_codes){
-						move_list->moves[move_list->count] = Move(normal_promoted, from, to, PAWN);
-						move_list->count++;
-					}
-				}
+		while (ep_squares != 0ULL){	
+			Square from = utils::pop_lsb(ep_squares);
+			Square ep_square = static_cast<Square>(state.enPassant);
 
-			}
+			move_list->moves[move_list->count] = Move(EN_PASSANT_FLAG, from, ep_square, PAWN, PAWN);
+			move_list->count++;		
 
-		
-			normal_moves = state.pieces.pawn_bb & ~RANK_2;
+		}	
 
-			while (normal_moves != 0ULL){
-				Square from = utils::pop_lsb(normal_moves);
-				uint64_t legalMove = (pawnAttackLookups[BLACK][from] & state.enemies );
+	}
+			
 
-				addPossibleMove<CAPTURES>(from, legalMove, PAWN, move_list);
+	else {
+		normal_moves = state.pieces.pawn_bb & ~RANK_2;
 
-			}	
+		while (normal_moves != 0ULL){
+			Square from = utils::pop_lsb(normal_moves);
+			uint64_t legalMove = (pawnAttackLookups[BLACK][from] & state.enemies );
 
-			promotions = state.pieces.pawn_bb & RANK_2;
+			addPossibleMove<CAPTURES>(from, legalMove, PAWN, move_list);
 
-			while (promotions != 0ULL){
-				Square from = utils::pop_lsb(promotions);
+		}	
 
-				uint64_t captures = pawnAttackLookups[BLACK][from] & state.enemies;
-				
-				while (captures != 0ULL)
-				{
-					Square to = utils::pop_lsb(captures);
+		promotions = state.pieces.pawn_bb & RANK_2;
 
-					for(auto& capture_promoted: capture_promoted_codes){
-						move_list->moves[move_list->count] = Move(capture_promoted, from, to, PAWN);
-						move_list->count++;
-					}	
+		while (promotions != 0ULL){
+			Square from = utils::pop_lsb(promotions);
+
+			uint64_t captures = pawnAttackLookups[BLACK][from] & state.enemies;
+			
+			while (captures != 0ULL)
+			{
+				Square to = utils::pop_lsb(captures);
+
+				for(auto& capture_promoted: capture_promoted_codes){
+					move_list->moves[move_list->count] = Move(capture_promoted, from, to, PAWN);
+					move_list->count++;
 				}	
-			}
-
-			// en passant	
-			while (ep_squares != 0ULL){	
-				Square from = utils::pop_lsb(ep_squares);
-				Square ep_square = static_cast<Square>(state.enPassant);
-
-				move_list->moves[move_list->count] = Move(EN_PASSANT_FLAG, from, ep_square, PAWN, PAWN);
-				move_list->count++;		
-
 			}	
+		}
 
-		
+	}
+	
 
 }
 
@@ -319,16 +344,25 @@ void MoveGen::generateRookMoves(MoveList* move_list){
 
         
 	while (rook_bb != 0ULL){
-                Square from = utils::pop_lsb(rook_bb);
+        Square from = utils::pop_lsb(rook_bb);
 
 
-                int index = utils::generateMagicIndex( all & rookOccupancyMasks[from], rookMagics[from], from, 0);
+        int index = utils::generateMagicIndex( all & rookOccupancyMasks[from], rookMagics[from], from, 0);
 	
 		uint64_t legalMove = rookMoveList[from][index] & ~(state.pieces.all); 
 
-		addPossibleMove<CAPTURES>(from, legalMove,  ROOK, move_list);
+		if (Type == CAPTURES)
+		{
+			addPossibleMove<CAPTURES>(from, legalMove & state.enemies, ROOK, move_list);
+		}
+			
+		else
+		{
+			addPossibleMove<QUIETS>(from, legalMove & ~state.enemies, ROOK, move_list);	
 
-        }
+		}
+
+    }
 	
 }
 
@@ -337,18 +371,27 @@ void MoveGen::generateBishopMoves(MoveList* move_list){
 	uint64_t all = state.pieces.all | state.enemies;
 	uint64_t bishop_bb = state.pieces.bishop_bb;
 
-        while (bishop_bb != 0ULL){
+	while (bishop_bb != 0ULL){
 
-                Square from = utils::pop_lsb(bishop_bb);
+		Square from = utils::pop_lsb(bishop_bb);
 
 
-                int index = utils::generateMagicIndex((all) & bishopOccupancyMasks[from], bishopMagics[from], from, 1);
+		int index = utils::generateMagicIndex((all) & bishopOccupancyMasks[from], bishopMagics[from], from, 1);
 
 		uint64_t legalMove = bishopMoveList[from][index] & ~(state.pieces.all); 
 
-                addPossibleMove<CAPTURES>(from, legalMove,  BISHOP, move_list);
+		if (Type == CAPTURES)
+		{
+			addPossibleMove<CAPTURES>(from, legalMove & state.enemies, BISHOP, move_list);
+		}
+				
+		else
+		{
+			addPossibleMove<QUIETS>(from, legalMove & ~state.enemies, BISHOP, move_list);	
 
-        }
+		}
+
+	}
 
 }
 
@@ -359,18 +402,28 @@ void MoveGen::generateQueenMoves(MoveList* move_list){
 		uint64_t queen_bb = state.pieces.queen_bb;
 
         while (queen_bb != 0ULL){
-                Square from = utils::pop_lsb(queen_bb);
+			Square from = utils::pop_lsb(queen_bb);
 
-                int rookIndex = utils::generateMagicIndex((all) & rookOccupancyMasks[from], rookMagics[from], from, 0);
+			int rookIndex = utils::generateMagicIndex((all) & rookOccupancyMasks[from], rookMagics[from], from, 0);
 
-                int bishopIndex = utils::generateMagicIndex((all) & bishopOccupancyMasks[from], bishopMagics[from], from, 1);
+			int bishopIndex = utils::generateMagicIndex((all) & bishopOccupancyMasks[from], bishopMagics[from], from, 1);
 
 
-		uint64_t legalBishopMoves = bishopMoveList[from][bishopIndex] & ~(state.pieces.all);
-        uint64_t legalRookMoves = rookMoveList[from][rookIndex] & ~(state.pieces.all);
+			uint64_t legalBishopMoves = bishopMoveList[from][bishopIndex] & ~(state.pieces.all);
+			uint64_t legalRookMoves = rookMoveList[from][rookIndex] & ~(state.pieces.all);
 
-		uint64_t legalQueenMoves = legalRookMoves | legalBishopMoves;
-                addPossibleMove<Type>(from, (legalQueenMoves) ,  QUEEN, move_list);
+			uint64_t legalQueenMoves = legalRookMoves | legalBishopMoves;
+                
+			if (Type == CAPTURES)
+			{
+				addPossibleMove<CAPTURES>(from, legalQueenMoves & state.enemies, QUEEN, move_list);
+			}
+				
+			else
+			{
+				addPossibleMove<QUIETS>(from, legalQueenMoves & ~state.enemies, QUEEN, move_list);	
+
+			}
 
         }
 
